97ba9a81e2ef0c2e1021183115f0b3203c78edfc
Gabriela Pantoja <pantoja24gabriela@gmail.com>
2021-11-08 22:22:29 -0300

    iniciado o trabalho

--- App.js
+++ App.js
@@ -1,12 +1,43 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
+import React, { useState, useEffect } from 'react';
+import { Platform, Text, View, StyleSheet } from 'react-native';
+import * as Location from 'expo-location';
+import api from './services/api'
+//const response = await fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${location.coords.latitude}&lon=${location.coords.longitude}&exclude={part}&appid=${API_KEY}&units=metric`);
+//199bdc2f065edbb48b1d203a64a0c4bf
+//https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}
+import axios from "axios";
 export default function App() {
+  const [location, setLocation] = useState(null);
+  const [errorMsg, setErrorMsg] = useState(null);
+  const [tempo, setTempo] = useState({});
+
+  useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        setErrorMsg('Permission to access location was denied');
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      setLocation(location);
+      if (location){
+        try { 
+          const response = await api.get(`onecall?lat=${location.coords.latitude}&lon=${location.coords.longitude}&exclude={part}&lang=pt_br&units=metric&appid=199bdc2f065edbb48b1d203a64a0c4bf`)
+          console.log(response.data)
+        } catch (error) {
+          console.log(error)
+        }
+      }
+    })();
+    
+  }, []);
+
+  
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+     
     </View>
   );
 }
