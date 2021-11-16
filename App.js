3468870c8e7dc78ead45d1efb5932924b889a749
Gabriela Pantoja <pantoja24gabriela@gmail.com>
2021-11-15 23:15:38 -0300

    finalizado

--- App.js
+++ App.js
@@ -2,9 +2,6 @@ import React, { useState, useEffect } from 'react';
 import { Platform, Text, View, StyleSheet } from 'react-native';
 import * as Location from 'expo-location';
 import api from './services/api'
-//const response = await fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${location.coords.latitude}&lon=${location.coords.longitude}&exclude={part}&appid=${API_KEY}&units=metric`);
-//199bdc2f065edbb48b1d203a64a0c4bf
-//https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}
 import axios from "axios";
 export default function App() {
   const [location, setLocation] = useState(null);
@@ -24,7 +21,8 @@ export default function App() {
       if (location){
         try { 
           const response = await api.get(`onecall?lat=${location.coords.latitude}&lon=${location.coords.longitude}&exclude={part}&lang=pt_br&units=metric&appid=199bdc2f065edbb48b1d203a64a0c4bf`)
-          console.log(response.data)
+          const { current } = response.data;
+          setTempo(current);
         } catch (error) {
           console.log(error)
         }
@@ -33,11 +31,13 @@ export default function App() {
     
   }, []);
 
-  
 
   return (
     <View style={styles.container}>
-     
+     <Text>Temperatura:{tempo.temp}</Text>
+     <Text>Sensação Termica:{tempo.feels_like}</Text>
+     <Text>Umidade:{tempo.humidity}</Text>
+     <Text>{tempo.weather[0].description}</Text>
     </View>
   );
 }
